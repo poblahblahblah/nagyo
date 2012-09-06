3479c3eef58210a7592035336856e4710ed34994
Patrick O'Brien <obrien.patrickl@gmail.com>
2012-09-05 20:53:21 -0700

    updating hardwareprofiles mcv for mongoid

--- nagyo-server/app/models/hardwareprofile.rb
+++ nagyo-server/app/models/hardwareprofile.rb
@@ -1,18 +1,22 @@
 class Hardwareprofile
-  include MongoMapper::Document
+  include Mongoid::Document
+  include Mongoid::Timestamps
+  include Mongoid::Fields
 
+  # scopes
   scope :hardware_profile,	proc {|hardware_profile| where(:hardware_profile => hardware_profile) }
   scope :check_commands,	proc {|check_commands| where(:check_commands => check_commands) }
   scope :contacts,              proc {|contacts| where(:contacts => contacts) }
 
-  before_save :reject_empty_inputs
+  # validations
+  before_save 			:reject_empty_inputs
+  validates_presence_of		:hardware_profile, :check_commands, :contacts
+  validates_uniqueness_of	:hardware_profile
 
   # required:
-  key :hardware_profile,	String,	:required => true, :unique => true
-  key :check_commands,		Array, 	:required => true
-  key :contacts,                Array,  :required => true
-
-  timestamps!
+  field :hardware_profile,	type: String
+  field :check_commands,	type: Array
+  field :contacts,		type: Array
 
   def initialize(*params)
     super(*params)
