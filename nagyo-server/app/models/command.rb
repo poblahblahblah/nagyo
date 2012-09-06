c4296767ad65069ed2a57bacae74c9a1e39ddb31
Patrick O'Brien <obrien.patrickl@gmail.com>
2012-09-05 19:32:12 -0700

    updating command model to use validates_presence_of rather than the (not working) required: true

--- nagyo-server/app/models/command.rb
+++ nagyo-server/app/models/command.rb
@@ -6,10 +6,11 @@ class Command
   scope :command_name, proc {|command_name| where(:command_name => command_name) }
 
   validates_uniqueness_of :command_name
+  validates_presence_of   :command_name, :command_line
 
   # required:
-  field :command_name, type: String, required: true
-  field :command_line, type: String, required: true
+  field :command_name, type: String
+  field :command_line, type: String
 
   def initialize(*params)
     super(*params)
