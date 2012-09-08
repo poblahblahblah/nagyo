51c3b192e2cde235987a7a439735c986ad61cc72
Damian Martinez <damian-gh@malachiarts.com>
2012-09-07 22:47:04 -0700

    Switch to rspec.

--- nagyo-server/config/application.rb
+++ nagyo-server/config/application.rb
@@ -1,12 +1,10 @@
 require File.expand_path('../boot', __FILE__)
 
 # Pick the frameworks you want:
-# require "active_record/railtie"
 require "action_controller/railtie"
 require "action_mailer/railtie"
 require "active_resource/railtie"
 require "sprockets/railtie"
-require "rails/test_unit/railtie"
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
@@ -43,7 +41,7 @@ module Nagyo
     config.encoding = "utf-8"
 
     # Configure sensitive parameters which will be filtered from the log file.
-    config.filter_parameters += [:password]
+    config.filter_parameters += [:password, :password_confirmation, :auth_token]
 
     # Enable the asset pipeline
     config.assets.enabled = true
--- nagyo-server/test/functional/host_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class HostControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
