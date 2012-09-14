74253feb156a8fa2e0ffc34b277f0ee9fa570d30
Damian Martinez <damian-gh@malachiarts.com>
2012-09-13 17:29:48 -0700

    Fix bug in nagios_options_from_array to generate all combinations of all lengths.

--- nagyo-server/app/helpers/application_helper.rb
+++ nagyo-server/app/helpers/application_helper.rb
@@ -8,14 +8,19 @@ module ApplicationHelper
   #   => %w{ a
   #          a,b
   #          a,b,c
+  #          a,c
   #          b
   #          b,c
   #          c     }
   #
-  def nagios_options_from_array(array)
+  # TODO: maybe a different interface for selection is more appropriate: try an 
+  # embedded document? Array of Strings? and make checkboxes for each possible 
+  # option, just selecting/deselecting and then collapsing to comma-sep string
+  #
+  def nagios_options_from_array(input)
     options = []
-    (0..array.length).to_a.combination(2).map{|i,j| array[i...j]}.sort.each do |a|
-      options << a.join(',')
+    (1..input.length).to_a.each do |k|
+      options += input.sort.combination(k).to_a.collect {|x| x.join(',') }
     end
     return options
   end
