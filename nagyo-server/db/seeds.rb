984dcc0818b2998a908993b40f274c2867978e80
Damian Martinez <damian-gh@malachiarts.com>
2012-09-24 14:26:57 -0700

    Add default timeperiods and commands to db:seed

--- nagyo-server/db/seeds.rb
+++ nagyo-server/db/seeds.rb
@@ -7,13 +7,43 @@
 #   Mayor.create(:name => 'Emanuel', :city => cities.first)
 #
 
+
 # This creates a default User called user@test.com, password: please
 #     not friendly with duplicates
 
 begin
-  FactoryGirl.create(:user)
+  factory_user = FactoryGirl.build(:user)
+  db_user = User.find_or_create_by(:email => factory_user.email)
+  if db_user
+    db_user.update_attributes(factory_user.attributes)
+    db_user.save
+  end
 rescue
   puts "Unable to create or have already created default user: #{$!}"
 end
 
+# timeperiods
+timeperiods = FactoryGirl.factories.select { |x| x.send(:class_name).to_s.downcase == "timeperiod" }
+timeperiods.each do |factory|
+  FactoryGirl.create(factory.name) rescue nil
+end
 
+# commands
+commands = FactoryGirl.factories.select { |x| x.send(:class_name).to_s.downcase == "command" }
+commands.each do |factory|
+  begin
+    factory_command = FactoryGirl.build(factory.name)
+    db_command = Command.find_or_create_by(:command_name => factory_command.command_name)
+    # don't want to update all opts - just command_line ?
+    if db_command
+      db_command.update_attributes(:command_line => factory_command.command_line)
+      db_command.save
+    end
+  rescue
+    puts "Unable to create or have already created command '#{factory_command}': #{$!}"
+  end
+end
+
+
+# TODO: is db seed_fu a better thing than factory girl for this usage?
+# factory-girl cannot easily update existing records :(
--- nagyo-server/spec/factories/commands.rb
+++ nagyo-server/spec/factories/commands.rb
@@ -24,52 +24,52 @@ FactoryGirl.define do
 
     factory :command_check_host_alive do
       command_name "check-host-alive"
-      command_line ""
+      command_line "check-host-alive"
     end
 
     factory :command_notify_host_by_email do
       command_name "notify-host-by-email"
-      command_line ""
+      command_line "notify-host-by-email"
     end
 
     factory :command_notify_service_by_email do
       command_name "notify-service-by-email"
-      command_line ""
+      command_line "notify-service-by-email"
     end
 
     factory :command_process_host_perfdata do
       command_name "process-host-perfdata"
-      command_line ""
+      command_line "process-host-perfdata"
     end
 
     factory :command_process_service_perfdata do
       command_name "process-service-perfdata"
-      command_line ""
+      command_line "process-service-perfdata"
     end
 
     factory :command_check_udp do
       command_name "check_udp"
-      command_line ""
+      command_line "check_udp"
     end
 
     factory :command_check_tcp do
       command_name "check_tcp"
-      command_line ""
+      command_line "check_tcp"
     end
 
     factory :command_check_snmp do
       command_name "check_snmp"
-      command_line ""
+      command_line "check_snmp"
     end
 
     factory :command_check_ping do
       command_name "check_ping"
-      command_line ""
+      command_line "check_ping"
     end
 
     factory :command_check_ssh do
       command_name "check_ssh"
-      command_line ""
+      command_line "check_ssh"
     end
 
 
