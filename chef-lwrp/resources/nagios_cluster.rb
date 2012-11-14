d681ae8adedc442ac7019cb27ce768becee940f5
Damian Martinez <damian-gh@malachiarts.com>
2012-11-14 11:35:42 -0800

    Move vip params into cluster

--- chef-lwrp/resources/nagios_cluster.rb
+++ chef-lwrp/resources/nagios_cluster.rb
@@ -1,9 +1,15 @@
 @@my_attrs = {
   :nodegroup =>                   {:kind_of => String, :required => true},
+  :vip_name =>                    {:kind_of => String, :required => true},
+  :vip_dns =>                     {:kind_of => String, :required => true},
+  :percent_warn =>                {:kind_of => String, :required => true},
+  :percent_crit =>                {:kind_of => String, :required => true},
+  :ecv_uri =>                     {:kind_of => String, :required => true},
+  :ecv_string =>                  {:kind_of => String, :required => true},
   :contacts =>                    {:kind_of => String, :required => true},
-  :check_command =>               {:kind_of => String, :required => true, :default => "check_eh_cluster-http"},
+  :check_command =>               {:kind_of => String, :required => false, :default => "check_eh_cluster-http"},
   :check_command_arguments =>     {:kind_of => String, :required => true},
-  :notify_on_node_service =>      {:kind_of => String, :required => false, :default => 'false'},
+  :notify_on_node_service =>      {:kind_of => String, :required => false, :default => 1},
   :node_check_command =>          {:kind_of => String},
   :node_check_command_arguments =>{:kind_of => String}
 }
@@ -19,8 +25,10 @@ actions :add
 @@my_attrs.each do |attr, options|
   attribute attr, options
 end
+
 attribute :my_attr_keys, :kind_of => Array, :required => false, :default => @@my_attrs.keys
-attribute :get_attrs, :kind_of => Array, :required => false, :default => [:nodegroup, :check_command, :contacts]
+attribute :get_attrs, :kind_of => Array, :required => false, :default => [:nodegroup, :check_command, :contacts, :vip_dns, :vip_name]
 attribute :nagyo_url, :required => false
 attribute :model_name, :required => false, :default => "cluster"
 attribute :model_name_plural, :required => false, :default => "clusters"
+
