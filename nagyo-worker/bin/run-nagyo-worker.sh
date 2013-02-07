470638e74a799b74c2eaaa5f92b23d9253d626af
Damian Martinez <damian-gh@malachiarts.com>
2013-02-07 09:11:39 -0800

    Pass extra command line options on to nagyo-worker.rb

--- nagyo-worker/bin/run-nagyo-worker.sh
+++ nagyo-worker/bin/run-nagyo-worker.sh
@@ -2,13 +2,17 @@
 
 # nagyo-worker.rb is a ruby script and the incantation is a bit involved - 
 # provide this utility script to run with production options
+#
+# Usage:
+#    sudo run-nagyo-worker.sh  [nagyo worker opts]
+#
 
 export PATH=/usr/local/rvm/bin:$PATH
 
 export NAGYO_WORKER_PATH=${NAGYO_WORKER_PATH:-"/data/svc/ops/nagyo/nagyo-worker"}
 export NAGYO_WORKER_GEMSET=${NAGYO_WORKER_GEMSET:-"ruby-1.8.7@nagyo-worker"}
 
-export NAGYO_WORKER_OPTS=${NAGYO_WORKER_OPTS:-"--sync --debug --config nagyo-worker.yml"}
+export NAGYO_WORKER_OPTS=${NAGYO_WORKER_OPTS:-"--sync --debug"}
 ###
 
 cd $NAGYO_WORKER_PATH || exit 1
@@ -19,4 +23,4 @@ if [[ ! -f $worker_script ]] ; then
   exit 3
 fi
 
-/usr/local/rvm/bin/rvm $NAGYO_WORKER_GEMSET do bundle exec ./bin/nagyo-worker.rb $NAGYO_WORKER_OPTS
+echo /usr/local/rvm/bin/rvm $NAGYO_WORKER_GEMSET do bundle exec ./bin/nagyo-worker.rb --config nagyo-worker.yml $NAGYO_WORKER_OPTS $*
